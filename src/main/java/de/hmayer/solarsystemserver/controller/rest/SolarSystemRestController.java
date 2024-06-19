9b248403c825929ccb58dff3c5476a8bb2762222
Holger-Mayer <60851489+Holger-Mayer@users.noreply.github.com>
2024-06-19 13:04:34 +0200

    SOL-4 Moved Rest-API to /api in Rest project and karate test project

--- src/main/java/de/hmayer/solarsystemserver/controller/rest/SolarSystemRestController.java
+++ src/main/java/de/hmayer/solarsystemserver/controller/rest/SolarSystemRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -26,6 +27,7 @@ import de.hmayer.solarsystemserver.model.MoonRepository;
 import de.hmayer.solarsystemserver.model.NamedObject;
 
 @CrossOrigin(origins = { "http://127.0.0.1:5500", "http://localhost:5500" })
+@RequestMapping("/api/")
 @RestController
 public class SolarSystemRestController {
 
